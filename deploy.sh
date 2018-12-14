07d2d688bd55718e9eed9fdc529c6d2f8a5c594b
Hugo Cortes <hcortes@mydevices.com>
2018-12-14 00:15:35 -0800

    cicd: travis docker building

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOCKER_HUB=hugocortes/me
+DATE=$(date +"%Y%m%d")
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker build -t $DOCKER_HUB:latest .
+docker tag $DOCKER_HUB:latest $DOCKER_HUB:release-$DATE-${TRAVIS_COMMIT:0:8}
+
+docker push $DOCKER_HUB
